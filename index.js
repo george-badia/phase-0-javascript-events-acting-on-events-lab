e49dc2347d3ae236578862c6860eb3e1c8f28bba
jojo <georgebadia2019@gmail.com>
2024-06-23 16:26:10 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function moveDodgerLeft() {
+   const dodger = document.getElementById('dodger');
+   let left = parseInt(dodger.style.left);
+   if (left > 0) {
+       dodger.style.left = `${left - 1}px`;
+   }
+}
+
+function moveDodgerRight() {
+   const dodger = document.getElementById('dodger');
+   let left = parseInt(dodger.style.left);
+   if (left < 360) {
+       dodger.style.left = `${left + 1}px`;
+   }
+}
